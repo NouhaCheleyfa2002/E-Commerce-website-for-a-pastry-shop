7b88c6cb1773341890b2852249d92b9ed07d1074
Nouha <nouhachelayfa@gmail.com>
2025-04-06 16:20:07 +0100

    Initial commit

--- client/src/components/AdminDashboard/AdminDashboard.jsx
+++ client/src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,29 +1,94 @@
-import "./AdminDashboard.css";
+import React from 'react';
+import { Box, useTheme } from "@mui/material";
+import { useGetAdminsQuery } from 'state/api';
+import { DataGrid } from '@mui/x-data-grid';
+import Header from 'components/Header';
 
-const Dashboard = () => {
+const Admin = () => {
+  const theme = useTheme()
+  const { data, isLoading } = useGetAdminsQuery();
+  
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
   return (
-    <div className="dashboard-overview">
-      <h1>Admin Dashboard</h1>
-      <div className="dashboard-cards">
-        <div className="card">
-          <h2>Products</h2>
-          <p>120</p>
-        </div>
-        <div className="card">
-          <h2>Orders</h2>
-          <p>45</p>
-        </div>
-        <div className="card">
-          <h2>Users</h2>
-          <p>302</p>
-        </div>
-        <div className="card">
-          <h2>Revenue</h2>
-          <p>$12,340</p>
-        </div>
-      </div>
-    </div>
+    <Box m="1.5rem 2.5rem">
+      <Header title="ADMINS" subtitle="Managing admins and list of admins" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+        />
+      </Box>
+    </Box>
   );
 };
 
-export default Dashboard;
+export default Admin;
